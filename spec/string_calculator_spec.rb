f923aa8a787f5ab1f84952a3d653f68949e7dfc4
AryamaanScaler <aryamaan.jain@scaler.com>
2025-04-15 18:38:45 +0530

    test: added testcase for emptry string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe StringCalculator do
       expect(true).to eq(true)
     end
   end
+
+  describe '.add' do
+    context 'when empty string is passed' do
+      it 'should return 0' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+  end
 end
