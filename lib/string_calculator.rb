b130e2258a6006c16da85b8ad129d727a019cc30
AryamaanScaler <aryamaan.jain@scaler.com>
2025-04-15 19:19:25 +0530

    refactor: parsing logic

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,19 +2,29 @@
 
 # StringCalculator
 class StringCalculator
+  DEFAULT_DELIMITER = ','
+
   def self.add(input)
     raise ArgumentError unless input.is_a? String
 
     return 0 if input.nil? || input.empty?
 
-    if input.start_with?('//')
-      delimiter_line, numbers_string = input.split("\n", 2)
-      custom_delimiter = delimiter_line[2]
-      numbers = numbers_string.split(/#{Regexp.escape(custom_delimiter)}|\n/).map(&:to_i)
-    else
-      numbers = input.split(/[,\n]/).map(&:to_i)
-    end
+    numbers = parse_numbers(input)
 
     numbers.inject(:+)
   end
+
+  def self.parse_numbers(input)
+    delimiter = DEFAULT_DELIMITER
+
+    if input.start_with?('//')
+      delimiter_line, numbers_string = input.split("\n", 2)
+      delimiter = delimiter_line[2]
+      input = numbers_string
+    end
+
+    input.split(/#{Regexp.escape(delimiter)}|\n/).map(&:to_i)
+  end
+
+  private_class_method :parse_numbers
 end
