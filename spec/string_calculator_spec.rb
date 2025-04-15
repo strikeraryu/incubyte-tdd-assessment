46bd4f7e3062401ec9db697050957e6927417d63
AryamaanScaler <aryamaan.jain@scaler.com>
2025-04-15 18:32:53 +0530

    test: added initial rspec

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require './lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  context 'smoke test' do
+    it 'Rspec file is working' do
+      expect(true).to eq(true)
+    end
+  end
+end
