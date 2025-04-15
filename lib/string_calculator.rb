9edb84543c93c99107e1db4b639fb2cdc64f1a4d
AryamaanScaler <aryamaan.jain@scaler.com>
2025-04-15 20:09:47 +0530

    fix: handle testcase for multi deli

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -18,17 +18,18 @@ class StringCalculator
   end
 
   def self.parse_numbers(input)
-    delimiter = DEFAULT_DELIMITER
+    delimiters = [DEFAULT_DELIMITER]
 
     if input.start_with?('//')
       delimiter_line, numbers_string = input.split("\n", 2)
-      match = delimiter_line.match(%r{\A//\[(.+)\]})
-      delimiter = match ? match[1] : delimiter_line[2]
+      delimiters = delimiter_line.scan(/\[(.*?)\]/).flatten
+      delimiters = delimiters.empty? ? [delimiter_line[2]] : delimiters
 
       input = numbers_string
     end
 
-    input.split(/#{Regexp.escape(delimiter)}|\n/).map(&:to_i)
+    delimiter_regex = delimiters.map { |d| Regexp.escape(d) }.join('|')
+    input.split(/#{delimiter_regex}|\n/).map(&:to_i)
   end
 
   private_class_method :parse_numbers
